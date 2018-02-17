8f85ed6d00770885a2bec8e66280ffab80b95653
Alex Lynd <alexlynd@protonmail.com>
2018-02-16 23:08:36 -0800

    new

--- Chapter 5/rock_paper_scissors.java
+++ Chapter 5/rock_paper_scissors.java
@@ -1,55 +1,29 @@
 import java.util.*;
 public class rock_paper_scissors {
     public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
-        
+        Scanner scan= new Scanner(System.in);
+        Random rand= new Random();
+        int wins=0;int losses=0;int ties=0;
+        int human=0;
+        String input="";
+        final int rock= 1, paper= 2, scissors= 3;
         System.out.println("Type \'quit\' to quit.");
-        String input = scan.next();
-        int random = rand.nextInt(3)+1;
-        int win=0;
-        int tie=0;
-        int loss=0;
-        //1=rock
-        //2=paper
-        //3=scissors
         while(!input.equalsIgnoreCase("quit")){
-            //rock cases
-            if (input.equalsIgnoreCase("rock") && random==1){
-                System.out.println("Rock, it was a tie");
-                tie++;
-            }else if(input.equalsIgnoreCase("paper") && random==1){
-                System.out.println("rock, you win");
-                win++;
-            }else if(input.equalsIgnoreCase("scissors") && random==1){
-                System.out.println("rock, i win!");
-                loss++;
-            }
-            //paper cases
-            if (input.equalsIgnoreCase("rock") && random==2){
-                System.out.println("Paper, i win!");
-                loss++;
-            }else if(input.equalsIgnoreCase("paper") && random==2){
-                System.out.println("Paper, it was a tie");
-                tie++;
-            }else if(input.equalsIgnoreCase("scissors") && random==2){
-                System.out.println("Paper, i lose...");
-                win++;
-            }
-            //scissors cases
-            if (input.equalsIgnoreCase("rock") && random==3){
-                System.out.println("Scissors, i lose...");
-                win++;
-            }else if(input.equalsIgnoreCase("paper") && random==3){
-                System.out.println("Scissors, i win!");
-                loss++;
-            }else if(input.equalsIgnoreCase("scissors") && random==3){
-                System.out.println("Scissors, it was a tie");
-                tie++;
-            }
+            System.out.print("Your weapon: ");          
             input = scan.next();
-            random=rand.nextInt(2)+1;
+            int computer=rand.nextInt(3)+1;
+            if (computer==1) System.out.println("Computer: "+ "Rock");
+            else if (computer==2) System.out.println("Computer: "+"Paper");
+            else System.out.println("Computer: "+"Scissors");
+            if (input.equalsIgnoreCase("rock")) human=1;
+            if (input.equalsIgnoreCase("paper")) human= 2;
+            if (input.equalsIgnoreCase("scissors")) human= 3;
+            if (human== computer){System.out.println("It's a tie!"); ties++;}
+            else if ((human==rock && computer==scissors)||(human==paper && computer==rock)||(human==scissors && computer==paper))
+            {System.out.println("You win!");wins++;}
+            else {System.out.println("You lose.");losses++;}
+			System.out.println("-------------------------------------------");
         }
-        System.out.println("Wins: "+win+"\nLosses: "+loss+"\nTies: "+tie);
+        System.out.println("Wins: "+wins+"\nLosses: "+losses+"\nTies: "+ties);
     }
 }
