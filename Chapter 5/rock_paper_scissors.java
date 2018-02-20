c7ed33cf6fb2c3f8a78b6142c2aa8886f60059d7
Alex Lynd <alexlynd@protonmail.com>
2018-02-20 11:43:31 -0800

    foo

--- Chapter 5/rock_paper_scissors.java
+++ Chapter 5/rock_paper_scissors.java
@@ -5,25 +5,29 @@ public class rock_paper_scissors {
         Random rand= new Random();
         int wins=0;int losses=0;int ties=0;
         int human=0;
-        String input="";
+        String input="";String computer_weapon="";
         final int rock= 1, paper= 2, scissors= 3;
         System.out.println("Type \'quit\' to quit.");
         while(!input.equalsIgnoreCase("quit")){
             System.out.print("Your weapon: ");          
             input = scan.next();
-            int computer=rand.nextInt(3)+1;
-            if (computer==1) System.out.println("Computer: "+ "Rock");
-            else if (computer==2) System.out.println("Computer: "+"Paper");
-            else System.out.println("Computer: "+"Scissors");
-            if (input.equalsIgnoreCase("rock")) human=1;
-            if (input.equalsIgnoreCase("paper")) human= 2;
-            if (input.equalsIgnoreCase("scissors")) human= 3;
-            if (human== computer){System.out.println("It's a tie!"); ties++;}
-            else if ((human==rock && computer==scissors)||(human==paper && computer==rock)||(human==scissors && computer==paper))
-            {System.out.println("You win!");wins++;}
-            else {System.out.println("You lose.");losses++;}
-			System.out.println("-------------------------------------------");
+            if (!input.equalsIgnoreCase("quit")){
+                int computer=rand.nextInt(3)+1;
+                if (computer==1) computer_weapon= "rock";
+                else if (computer==2) computer_weapon= "paper";
+                else computer_weapon= "scissors";
+                System.out.println("Computer: "+computer_weapon);
+                if (input.equalsIgnoreCase("rock")) human=1;
+                if (input.equalsIgnoreCase("paper")) human= 2;
+                if (input.equalsIgnoreCase("scissors")) human= 3;
+                if (human== computer){System.out.println("It's a tie!"); ties++;}
+                else if ((human==rock && computer==scissors)||(human==paper && computer==rock)||(human==scissors && computer==paper))
+                {System.out.println("You win!");wins++;}
+                else {System.out.println("You lose.");losses++;}
+            }
+            System.out.println("-------------------------------------------");
         }
+        System.out.println("Program terminated.");
         System.out.println("Wins: "+wins+"\nLosses: "+losses+"\nTies: "+ties);
     }
 }
