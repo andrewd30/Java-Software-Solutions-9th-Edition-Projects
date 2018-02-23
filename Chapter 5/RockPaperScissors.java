141ba043def60ac1a6fe22a22cd50a0b9b5c2204
Alex Lynd <alexlynd@protonmail.com>
2018-02-22 21:58:17 -0800

    Renames

--- Chapter 5/RockPaperScissors.java
+++ Chapter 5/RockPaperScissors.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class rock_paper_scissors {
+public class RockPaperScissors {
     public static void main(String[]args){
         Scanner scan= new Scanner(System.in);
         Random rand= new Random();
